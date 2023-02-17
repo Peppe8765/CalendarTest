722e20426d1124960bfba5d517dddf3d9a3fe454
Peppe8765 <giuseppe.ferrante99@gmail.com>
2023-02-17 22:33:55 +0100

    prova commit 2

--- src/calendarTest/CalendarMain.java
+++ src/calendarTest/CalendarMain.java
@@ -1,5 +1,32 @@
 package calendarTest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class CalendarMain {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("inserisci un numero");
+		try {
+			//String s = br.readLine();
+			int s = br.read();
+			s++;
+			System.out.println(s);
+			
+		}catch (Exception e) {
+			System.out.println("lanciata eccezione");
+		}
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			int a = scanner.nextInt();
+			System.out.println(a);
+		}
+		
+	}
 
 }
